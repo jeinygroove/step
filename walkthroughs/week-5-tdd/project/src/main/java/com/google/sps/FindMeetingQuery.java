7700558b2129644729f4ac0dec7423bb5ab65aaa
Olga Shimanskaia <oshimanskaia@google.com>
2020-07-28 02:13:11 +0300

    added first version of algorithm without optional attendees

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,61 @@
 
 package com.google.sps;
 
-import java.util.Collection;
+import java.util.*;
 
 public final class FindMeetingQuery {
+
+  /**
+   * Returns collection of time slots, in which we can appoint the meeting.
+   * @param events   All events in the day.
+   * @param request  Information about event that we want to add (attendees, required duration).
+   * */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // hash map, where key is the time moment and object is the number of attendees from the request,
+    // who will be busy or free from that time moment
+    HashMap<Integer, Long> timePoints = new HashMap<>();
+    Collection<String> attendees = request.getAttendees();
+
+    long requiredAmount = attendees.size();
+    long requiredDuration = request.getDuration();
+
+    // add start and end times of the day
+    timePoints.put(0, requiredAmount);
+    timePoints.put(TimeRange.WHOLE_DAY.end(), -requiredAmount);
+
+    // add all time point, where the number of free attendees is changing
+    for (Event event : events) {
+      // the event can have attendees that are not present in the request,
+      // so we shouldn't count them
+      Collection<String> eventAttendees = new ArrayList<>(event.getAttendees());
+      eventAttendees.retainAll(attendees);
+
+      // if intersection isn't empty
+      if (!eventAttendees.isEmpty()) {
+        int startTime = event.getWhen().start();
+        int endTime = event.getWhen().end();
+
+        timePoints.put(startTime, timePoints.getOrDefault(startTime, 0L) - eventAttendees.size());
+        timePoints.put(endTime, timePoints.getOrDefault(endTime, 0L) + eventAttendees.size());
+      }
+    }
+
+    List<Integer> sortedTimes = new ArrayList<>(timePoints.keySet());
+    Collections.sort(sortedTimes);
+
+    long currentAmount = 0;
+    int previousTime = 0;
+    Collection<TimeRange> timeSlots = new ArrayList<>();
+
+    // find time slots with the help of scan line method
+    for (int currentTime : sortedTimes) {
+      if (currentAmount == requiredAmount && (currentTime - previousTime) >= requiredDuration) {
+        timeSlots.add(TimeRange.fromStartEnd(previousTime, currentTime, false));
+      }
+      currentAmount += timePoints.get(currentTime);
+      previousTime = currentTime;
+    }
+
+    return timeSlots;
   }
 }
