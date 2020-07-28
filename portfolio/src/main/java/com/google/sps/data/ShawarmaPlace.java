cd954acda979c0723e897d66cebf98b5ad1a7dc6
Olga Shimanskaia <oshimanskaia@google.com>
2020-07-28 12:56:09 +0300

    added ShawarmaPlace class documentation

--- portfolio/src/main/java/com/google/sps/data/ShawarmaPlace.java
+++ portfolio/src/main/java/com/google/sps/data/ShawarmaPlace.java
@@ -1,5 +1,9 @@
 package com.google.sps.data;
 
+/**
+ * Represents a shawarma place (it's location, name and description about its shawarmas).
+ * @author Olga Shimanskaia <olgashimanskaia@gmail.com>
+ */
 public class ShawarmaPlace {
     private double latitude;
     private double longitude;
