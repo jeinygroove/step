88fb0a466ac7c61cca9dd180fcccece6a05a1322
Olga Shimanskaia <oshimanskaia@google.com>
2020-07-28 15:24:22 +0300

    clarified the usage of the ShawarmaPlace class in it's description

--- portfolio/src/main/java/com/google/sps/data/ShawarmaPlace.java
+++ portfolio/src/main/java/com/google/sps/data/ShawarmaPlace.java
@@ -2,6 +2,7 @@ package com.google.sps.data;
 
 /**
  * Represents a shawarma place (it's location, name and description about its shawarmas).
+ * This class is only used by GSON with the help of reflection.
  * @author Olga Shimanskaia <olgashimanskaia@gmail.com>
  */
 public class ShawarmaPlace {
